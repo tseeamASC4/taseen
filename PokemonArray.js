ab29add1f168afd2bec55471380012a2d0ff03ed
tseeamASC4 <seeamt@bxscience.edu>
2017-07-11 12:07:35 -0400

    first commit

---
+++ PokemonArray.js
@@ -0,0 +1,32 @@
+var myPokemonRoster = ["charizard", "eevee","empoleon","swampert","blaziken","lugia"];
+console.log (myPokemonRoster);
+var firstToFight= (myPokemonRoster[0])
+var secondToFight= (myPokemonRoster[1]);
+myPokemonRoster.pop()
+console.log(myPokemonRoster)
+myPokemonRoster.push("entei","scyther")
+console.log(myPokemonRoster)
+console.log(myPokemonRoster.length)
+RosterSize=(myPokemonRoster.length)
+console.log(RosterSize)
+
+/*for(var i=0; i<5 ; i++){
+    
+    console.log(myPokemonRoster[i])
+}
+*/
+
+for(var i=6; i>0 ; i--){
+    
+    console.log(myPokemonRoster[i])
+}
+
+/*for(var i=0; i<RosterSize ; i++){
+    
+    console.log(myPokemonRoster[i])
+}
+
+/*for(var i=0; i<5 ; i++){
+    
+    console.log(myPokemonRoster[i])
+} */
---
+++ loop.js
@@ -0,0 +1,4 @@
+var myPeeps = ["Taseen", "Tahmidur","rabbt"]
+for(var i=0;i<myPeeps.length;i++){
+    console.log(myPeeps[i]);
+}
