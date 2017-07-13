e09bc5cd0ffd7a6ea5af79049b3114706b357176
tseeamASC4 <seeamt@bxscience.edu>
2017-07-13 08:12:51 -0400

    added new files

--- pokemonfunctions.js
+++ pokemonfunctions.js
@@ -3,19 +3,19 @@ var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
 function randletter()
 {
     var result = Math.floor(Math.random() * alphabet.length);
-    return result;
+    return (alphabet[result]);
 }
 
-console.log(alphabet[randletter()]) 
+console.log(randletter()) 
 
 
 
 function randWord()
 {
     var word = "";
-    var wordl = Math.floor(Math.random()*25*Math.random())
-    for(var i= wordl; i > 0; i--)
-    {word === randletter() + word;}
+    var wordl = Math.floor(Math.random()*25)
+    for(var i = wordl; i > 0; i--)
+    word = randletter() + word;
     return word;
     
 }
@@ -23,3 +23,14 @@ function randWord()
 console.log(randWord())
 
 
+
+function randSentence()
+{
+    var sentence = "";
+    var sentencel = Math.floor(Math.random()* 30)
+    for(var i=sentencel; i>0; i--)
+    sentence= randWord() +  + sentence + ' ' + sentence ;
+    return sentence;
+} 
+
+console.log(randSentence())
