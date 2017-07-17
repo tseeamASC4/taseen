9c3f14f9319665edc7aa262f4e13d805c42452c3
tseeamASC4 <seeamt@bxscience.edu>
2017-07-17 13:10:31 -0400

    yourmessage

---
+++ firstp5/sketch.js
@@ -0,0 +1,6 @@
+
+function setup() {
+  createCanvas(1000, 1000);
+ellipse(200,200,200,200);
+ellipse(200,200,10,10);
+}
---
+++ pokeball/sketch.js
@@ -0,0 +1,8 @@
+function setup(){
+createCanvas(1000,1000);
+
+ellipse(200,200,200,200);
+fill(color(10,10,1))
+ellipse(200,200,50,50);
+
+}
