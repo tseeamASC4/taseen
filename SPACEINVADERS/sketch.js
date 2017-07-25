c074a70c837f42bb7d1be5c262d61197670a103b
tseeamASC4 <seeamt@bxscience.edu>
2017-07-25 12:04:28 -0400

    YUH

--- SPACEINVADERS/sketch.js
+++ SPACEINVADERS/sketch.js
@@ -1,45 +1,44 @@
-var sbutton;
-var canvas;
-function setup(){
-canvas = createCanvas(800,600);
-background('black');
-textSize(60);
-fill('white');
-text("S P A C E - I N V A D E R S",20,73)
-sbutton = createButton('START');
-sbutton.position(350,450)
-sbutton.size(100,50)
-}
-sbutton.mousePressed() {
-    clear()
-}
+var x = 600;
+var y = 700;
+ var sbutton;
+// var gameStarted = false
+var gamestarted = false
 
-
-
-
-
-
-var ship;
-var x;
-var y;
-function setup(){
-createCanvas(800,600)
-background('black')
-}
-
-function draw(){
-    ship()
-
-function ship(){
-x=400;
-y=550;
-ship= rect(x,y,90,90);
-
+    function draw(){
+if (gamestarted === false){
+        createCanvas(windowWidth,windowHeight);
+        background('black');
+        textSize(60);
+        fill('white');
+        text("S P A C E - I N V A D E R S",0,20,windowWidth,windowHeight/3)
+        // background('black');
+        sbutton = createButton('START');
+        sbutton.position(350,450)
+        sbutton.size(100,50)
+    function l(){
+        {
+            gamestarted = true;
+            console.log(gamestarted)
+        }
+      
+            
+         }
+sbutton.mouseClicked(l)
     }
-}
+    
+else
 
-function keyPressed(){
-    if(keyCode == LEFT_ARROW)
-    {ship.x = x+ 10}
-    else if(keyCode== RIGHT_ARROW)
-    {ship.x = x-10}}
+{
+    //function draw() {
+        if (keyIsDown(LEFT_ARROW) && x>0){
+            x-=10;
+        }
+        if (keyIsDown(RIGHT_ARROW) && x < 750){
+             x+=10;
+        }
+        clear();
+        background('black')
+        fill(0, 255, 0);
+        rect(x,y,50,50)
+}
+    }
